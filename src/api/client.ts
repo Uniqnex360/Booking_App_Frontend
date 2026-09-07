3b265436e7aa1beed303cd7097549caedbdebbe8
Harisankar <harisankar@uniqnex360.com>
2026-09-07 13:18:56 +0530

    Fix:Added vercel.json

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:9001/v1';
+const API_BASE_URL = import.meta.env.API_BASE_URL || 'http://localhost:9001/v1';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
