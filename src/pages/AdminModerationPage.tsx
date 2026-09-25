1d75334b32a5eea9b14c6e98f81ece34519d3678
Harisankar <harisankar@uniqnex360.com>
2026-09-25 15:26:55 +0530

    style(AdminModerationPage): reduce top padding for improved layout

--- src/pages/AdminModerationPage.tsx
+++ src/pages/AdminModerationPage.tsx
@@ -134,7 +134,7 @@ export default function AdminModerationPage() {
         </div>
       </div>
 
-      <div className="mx-auto max-w-6xl px-4 pt-32 pb-8 sm:px-6 lg:px-8">
+<div className="mx-auto max-w-6xl px-4 pt-8 pb-8 sm:px-6 lg:px-8">
 
         {/* Admin Navigation Tabs */}
         <div className="flex border-b border-slate-200/60 gap-4 mb-6">
